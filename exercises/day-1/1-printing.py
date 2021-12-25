d3ab706a3365ae13e1acc2594f368155a4088081
Argem Flores <argemflores@gmail.com>
2021-12-25 21:58:57 +0800

    Solve printing exercise

---
+++ exercises/day-1/1-printing.py
@@ -0,0 +1,14 @@
+"""
+Printing
+
+Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
+
+https://app.codingrooms.com/w/0dJKpT635cGt
+"""
+
+#Write your code below this line 👇
+
+# print strings
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
