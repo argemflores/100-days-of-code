b2305ef5f4c1b541560b25251fa50d580dfeebbe
Argem Flores <argemflores@gmail.com>
2021-12-25 21:59:22 +0800

    Solve variables exercise

---
+++ exercises/day-1/4-variables.py
@@ -0,0 +1,27 @@
+"""
+Variables
+
+Write a program that switches the values stored in the variables a and b.
+
+https://app.codingrooms.com/w/lr68OBDs9Fwe
+"""
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+# swap values with a third variable c
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
