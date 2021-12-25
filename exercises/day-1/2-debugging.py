510a6b770d79d9c25e4c86c66a7b927c3f27e721
Argem Flores <argemflores@gmail.com>
2021-12-25 21:59:05 +0800

    Solve debugging exercise

---
+++ exercises/day-1/2-debugging.py
@@ -0,0 +1,15 @@
+"""
+Debugging
+
+Look at the code in the code editor on the right. There are errors in all of the lines of code. Fix the code so that it runs without errors.
+
+https://app.codingrooms.com/w/Oig1Lj2eT6I5
+"""
+
+#Fix the code below 👇
+
+# fix errors in quotes, escaping, and extra parenthesis
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \"+\" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
