12548ad9763df2ecd063895a8965de1e5050b410
Argem Flores <argemflores@gmail.com>
2021-12-25 21:59:13 +0800

    Solve input exercise

---
+++ exercises/day-1/3-input-function.py
@@ -0,0 +1,12 @@
+"""
+Input function
+
+Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+https://app.codingrooms.com/w/6CQBc165ZaEc
+"""
+
+#Write your code below this line 👇
+
+# prompt string, then show character length
+print(len(input("What is your name? ")))
