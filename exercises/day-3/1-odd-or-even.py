64bcbc6b822c9d8036272f958d68f4df760fbd69
Argem Flores <argemflores@gmail.com>
2022-01-15 20:32:52 +0800

    Submit exercises to CodingRooms

--- exercises/day-3/1-odd-or-even.py
+++ exercises/day-3/1-odd-or-even.py
@@ -5,6 +5,6 @@ number = int(input("Which number do you want to check? "))
 #Write your code below this line 👇
 
 if number % 2 == 0:
-    print("Even")
+    print("This is an even number.")
 else:
-    print("Odd")
+    print("This is an odd number.")
--- exercises/day-3/2-bmi.py
+++ exercises/day-3/2-bmi.py
@@ -8,17 +8,14 @@ weight = float(input("enter your weight in kg: "))
 # Body Mass Index (BMI) = weight (kg) / height (m) squared
 bmi = round(weight / height ** 2)
 
-# print BMI results
-print(f"Your BMI is {bmi}.")
-
 # print BMI interpretation
 if bmi < 18.5:
-    print("You are underweight.")
+    print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi < 25:
-    print("You have normal weight.")
+    print(f"Your BMI is {bmi}, you have a normal weight.")
 elif bmi < 30:
-    print("You are overweight.")
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
 elif bmi < 35:
-    print("You are obese.")
+    print(f"Your BMI is {bmi}, you are obese.")
 else:
-    print("You are clinically obese.")
+    print(f"Your BMI is {bmi}, you are clinically obese.")
