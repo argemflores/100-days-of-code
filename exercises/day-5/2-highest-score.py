2b6dd0c4cf176ac9d01e811ff27851aa399ecd65
Argem Flores <argemflores@gmail.com>
2022-01-20 22:19:09 +0800

    Solve highest score exercise

---
+++ exercises/day-5/2-highest-score.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+highest_score = 0
+
+# find highest score
+for score in student_scores:
+    if score > highest_score:
+        # score is higher than current highest score
+        highest_score = score
+
+print(highest_score)
