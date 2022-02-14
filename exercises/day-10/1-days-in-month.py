c977ee678a4f217009c127680ec554bc605f2dc8
Argem Flores <argemflores@gmail.com>
2022-02-14 22:09:10 +0800

    Solve Days in Month exercise

---
+++ exercises/day-10/1-days-in-month.py
@@ -0,0 +1,33 @@
+# check if leap year
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+# return the number of days in a month, considering leap years
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    # invalid month
+    if month >= len(month_days) or month < 0:
+        return "Invalid input/s. Try again."
+
+    # leap year and February
+    if is_leap(year) and month == 2:
+        return 29
+    else:
+        # other year and month
+        return month_days[month - 1]
+
+#🚨 Do NOT change any of the code below
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
